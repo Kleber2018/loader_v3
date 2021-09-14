0530c87e3d0f88d15a1420ab5402b77fedf7b236
Kleber2018 <42260292+Kleber2018@users.noreply.github.com>
2021-09-13 23:24:45 -0300

    script de atualização

--- atualiza.py
+++ atualiza.py
@@ -3,6 +3,7 @@
 from datetime import datetime, timedelta
 import json
 import os
+import time
 
 def resetar_fila():
     try:
@@ -17,18 +18,57 @@ def resetar_fila():
         print('erro no git reset', e)
         return 0
 
-def atualizando():
+def volta_backup():
     try:
-        resetar_fila()
-        import time
-        time.sleep(5.0)
-        print("atualizando para nova versão")
-        c = os.popen(f"git pull")
+        print('fazendo backup')
+        c = os.popen(f"python3 ./../script_bkp.py")
         c.read()
         c.close()
         return 1
     except Exception as e:
+        print('erro no git backup', e)
+        return 0
+
+def atualizando():
+    try:
+        if backup() == 1:
+            time.sleep(10.0)
+            resetar_fila()
+            time.sleep(5.0)
+            print("atualizando para nova versão")
+            c = os.popen(f"git pull")
+            c.read()
+            c.close()
+        return 1
+    except Exception as e:
         print('erro atualização', e)
+        return 0
+
+def clear_bkp():
+    try:
+        now = datetime.now()
+        print(now.year)
+        c = os.popen(f"rm -fR ./../../loaders_bkp")
+        c.read()
+        c.close()
+        return 1
+    except Exception as e:
+        print('erro clear_bkp', e)
+        return 0
+
+def backup():
+    try:
+        clear_bkp()
+        import time
+        time.sleep(5.0)
+        now = datetime.now()
+        print(now.year)
+        c = os.popen(f"cp -fR ./../../loaders ./../../loaders_bkp")
+        c.read()
+        c.close()
+        return 1
+    except Exception as e:
+        print('erro backup2', e)
         return 0
 
 
